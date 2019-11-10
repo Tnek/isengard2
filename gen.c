b078fb19b109717944683a1daf5261aad492d022
Kent Ma <kent@osiris.cyber.nyu.edu>
2019-11-10 13:15:33 -0500

    Linting

--- gen.c
+++ gen.c
@@ -1,33 +1,30 @@
 #include "tree.h"
 
-void pre_order(node_t *root) 
-{
-	if (root != NULL) {
-        printf("%d ", root->val);
-        pre_order(root->left);
-        pre_order(root->right);
-	}
+void pre_order(node_t *root) {
+  if (root != NULL) {
+    printf("%d ", root->val);
+    pre_order(root->left);
+    pre_order(root->right);
+  }
 }
 
 int main() {
-    node_t* root = NULL;
-    char* random_key = "pm @tnek your complaints";
+  node_t *root = NULL;
+  char *random_key = "pm @tnek your complaints";
 
-    for (int i = 0; random_key[i] != '\x00'; i++) {
-        root = insert(root, random_key[i]);
-    }
+  for (int i = 0; random_key[i] != '\x00'; i++) {
+    root = insert(root, random_key[i]);
+  }
 
-    printf("Flag gen: ");
-    pre_order(root);
-    printf("\n");
+  printf("Flag gen: ");
+  pre_order(root);
+  printf("\n");
 
+  node_t *tree = NULL;
 
-    node_t *tree = NULL;
+  for (int i = 0; random_key[i] != '\x00'; i++) {
+    tree = insert(tree, random_key[i]);
+  }
 
-    for (int i = 0; random_key[i] != '\x00'; i++) {
-        tree = insert(tree, random_key[i]);
-    }
-
-    printf("%d ", test_flag(tree));
+  printf("%d ", test_flag(tree));
 }
- 
